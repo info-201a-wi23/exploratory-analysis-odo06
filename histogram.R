cde4e25fb514794ad5fe5cff5a37213920e99218
lzheng456 <leoivanzheng@gmail.com>
2023-02-20 13:43:43 -0800

    Chart 3 update

--- histogram.R
+++ histogram.R
@@ -25,7 +25,3 @@ hist <- ggplot(data = post2) +
   facet_wrap(~sex)
 
 hist
-
-count(dogs$breed_primary)
-
-# By splitting this histogram into different facets, we attempt to show the number of days that a dog typically remains in a shelter based on their gender Using a histogram helps visualize the frequency distribution of the dataset making it is easier to tell how many days are the most or least common for a dog to stay for. The graphs reveal that a majority of dogs stay for around 1200 days. Additionally, they are skewed right, meaning that the number of dogs that stay past 1200 days decreases as time goes on. Several possible outliers exist as well, which are most likely those that have stayed for over 6000 days. Another piece of information that this visualization expresses is that a dog's gender does not play a large role in its adoption since we see that the mode heavily outnumbers the other values. 
